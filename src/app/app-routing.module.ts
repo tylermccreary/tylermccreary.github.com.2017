abca1c55b50fb0bc9434a295328afa7b28feb1f1
tmccreary <tjm3225@truman.edu>
2017-05-02 16:35:22 -0700

    ie fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,8 @@ const routes: Routes = [
       { path: 'about', component: AboutComponent },
       { path: 'contact', component: ContactComponent },
       { path: 'experience', component: ExperienceComponent }
-    ]}
+    ]},
+    { path: '**', redirectTo: '/home'}
 
 ];
 @NgModule({
